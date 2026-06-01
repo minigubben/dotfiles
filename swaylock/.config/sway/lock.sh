5bee3e9f05187bcc234b2cea51829d81d06e9aeb
minigubben <hello@noahch.se>
2026-06-01 22:20:09 +0200

    Add swayidle and lock

---
+++ swaylock/.config/sway/lock.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+exec swaylock -f \
+  --screenshots \
+  --clock \
+  --indicator \
+  --indicator-radius 100 \
+  --indicator-thickness 7 \
+  --effect-blur 22x12 \
+  --effect-vignette 0.9:0.9 \
+  --fade-in 0.2 \
+  --ring-color bb00cc \
+  --key-hl-color 880033 \
+  --line-color 00000000 \
+  --inside-color 00000088 \
+  --separator-color 00000000 \
+  --text-color ffffff
