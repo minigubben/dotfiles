17e4f2d5f09e400207fa7f6aab73b5d7d5fda8bd
minigubben <hello@noahch.se>
2026-06-02 20:45:09 +0200

    Bind Super+L to screen lock
    - Add a lock command pointing to the sway lock script
    - Map `SUPER+L` in Hyprland to launch the locker

--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -257,6 +257,7 @@ hl.device({
 ---------------------
 
 local mainMod = "SUPER" -- Sets "Windows" key as main modifier
+local lockCommand = "$HOME/.config/sway/lock.sh"
 local screenshotCommand = "mkdir -p \"$HOME/Pictures/Screenshots\" && file=\"$HOME/Pictures/Screenshots/screenshot-$(date +%Y-%m-%d_%H-%M-%S).png\" && grim -g \"$(slurp)\" \"$file\" && wl-copy --type image/png < \"$file\""
 
 -- Example binds, see https://wiki.hypr.land/Configuring/Basics/Binds/ for more
@@ -269,6 +270,7 @@ hl.bind(mainMod .. " + V", hl.dsp.window.float({ action = "toggle" }))
 hl.bind(mainMod .. " + R", hl.dsp.exec_cmd(menu))
 hl.bind(mainMod .. " + P", hl.dsp.window.pseudo())
 hl.bind(mainMod .. " + J", hl.dsp.layout("togglesplit"))    -- dwindle only
+hl.bind(mainMod .. " + L", hl.dsp.exec_cmd(lockCommand))
 hl.bind("Print", hl.dsp.exec_cmd(screenshotCommand))
 
 -- Move focus with mainMod + arrow keys
